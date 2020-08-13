187bc900ebf772ec9e96b18e6673533039de6b38
dhelmy22 <64977520+dhelmy22@users.noreply.github.com>
2020-08-13 10:48:06 -0400

    initial update working save

--- src/components/Plants/Plant/PlantResults.jsx
+++ src/components/Plants/Plant/PlantResults.jsx
@@ -5,8 +5,14 @@ import FormLabel from "@material-ui/core/FormLabel";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import { useState } from "react";
+// import Plant from "../../../../../../server/Plantify-Server/server/controllers/plantcontroller";
 
 const PlantResults = (props) => {
+  
+
+  // console.log('plantResults',props.results[0]);
+
   const savePlant = (plant) => {
     console.log(plant);
 
@@ -18,28 +24,30 @@ const PlantResults = (props) => {
     // let genus = plant.genus;
     // let image = plant.image_url;
     // let owner = user.id;
-    const accessToken = localStorage.getItem("SessionToken");
+    const accessToken = localStorage.getItem("token");
 
     let newPlantData = {
       plant: {
-        id: plant.id,
+        // id: plant.id,
         commonName: plant.common_name,
         family: plant.family,
         familyCommonName: plant.family_common_name,
         scientificName: plant.scientific_name,
         genus: plant.genus,
         image: plant.image_url,
+        description: plant.description,
+        notes: plant.notes
       },
     };
     console.log(newPlantData);
 
-    fetch("http://localhost:3002/plant/create", {
+    fetch("https://plantify-server.herokuapp.com/plant/create", {
       method: "POST",
       headers: new Headers({
         "Content-Type": "application/json",
-        Authorization: accessToken,
+        "Authorization": accessToken,
       }),
-      body: JSON.stringify(plant),
+      body: JSON.stringify(newPlantData),
     })
       .then((response) => {
         console.log(response.json());
@@ -48,6 +56,32 @@ const PlantResults = (props) => {
         console.log(err);
       });
   };
+  // ----------------------------------------------------------------------------------
+  // const handleSubmit = (e) => {
+  //   // e.prevent.default();
+  //   fetch('https://plantify-server.herokuapp.com/plant/create', {
+  //     method: 'POST',
+  //     body: JSON.stringify({plant:{commonName: commonName, family:family, familyCommonName: familyCommonName, scientificName:scientificName, genus:genus, description:description, notes: notes}}),
+  //     headers: new Headers({
+  //       'Content-Type': 'application/json',
+  //       'Authorization': localStorage.getItem('token'),
+  //     })
+  //   }).then(res => res.json())
+  //     // .then(logData => {
+  //     //   console.log(logData);
+  //     //   setCommonName(plant.common_name);
+  //     //   setFamily(plant.family);
+  //     //   setFamilyCommonName(plant.family_common_name);
+  //     //   setScientificName(plant.scientific_name);
+  //     //   setGenus(plant.genus);
+  //     //   setDescription(plant.description);
+  //     //   setNotes(plant.notes);
+  //     //   // props.newPlantData();
+  //     // })
+
+  // }
+
+  // ----------------------------------------------------------------------------------
 
   return (
     <Grid container className="plants">
@@ -67,7 +101,7 @@ const PlantResults = (props) => {
                     src={plant.image_url}
                   />
                 </Paper>
-                <Button id="submit-button" onClick={() => savePlant(plant)}>Save</Button>
+                <Button type='submit' id="submit-button" onClick={props.handleSubmit}  >Save</Button>
               </Grid>
             ))}
         </Grid>
